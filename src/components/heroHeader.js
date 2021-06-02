ed41588f95a5f9c85ebcae908d2aa68ea294573c
marusho <mrsho34@gmail.com>
2021-06-02 15:13:27 +0900

    feat: comment out get in touch

--- src/components/heroHeader.js
+++ src/components/heroHeader.js
@@ -1,5 +1,5 @@
-import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
+import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
 export default () => (
   <StaticQuery
     query={graphql`
@@ -14,15 +14,18 @@ export default () => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <div className="hero-header">
         <div className="headline">{data.site.siteMetadata.home.title}</div>
-        <div 
-          className="primary-content" 
-          dangerouslySetInnerHTML={{ __html: data.site.siteMetadata.home.description}}
+        <div
+          className="primary-content"
+          dangerouslySetInnerHTML={{
+            __html: data.site.siteMetadata.home.description
+          }}
         />
-        <Link to='/contact' className="button -primary">Get in touch &rarr;</Link>
+        {/* Contact FormへのGet in Touch リンクを削除する*/}
+        {/* <Link to='/contact' className="button -primary">Get in touch &rarr;</Link> */}
       </div>
     )}
   />
-)
+);
