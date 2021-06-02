4f1c9a58fed40e361e7e05d285953746b25a0c9c
marusho <mrsho34@gmail.com>
2021-06-02 20:13:45 +0900

    fix: manifestファイルを修正

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,62 +5,62 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  siteMetadata: require("./site-meta-data.json"),
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/_data`
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-prismjs`,
-            options: {
-              classPrefix: "language-",
-              inlineCodeMarker: null,
-              aliases: {},
-              showLineNumbers: false,
-              noInlineHighlight: false
-            }
-          },
-          {
-            resolve: "gatsby-remark-emojis"
-          }
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        // The property ID; the tracking code won't be generated without it. replace with yours
-        trackingId: "UA-164289266-2",
-        head: true
-      }
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
-        start_url: `/`,
-        background_color: `#fff`,
-        theme_color: `#AEA851`,
-        display: `standalone`,
-        icon: "src/images/icon.png"
-      }
-    },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-netlify-cms`,
-    "gatsby-plugin-dark-mode",
-    // siteURL is a must for sitemap generation
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`
-  ]
+	/* Your site config here */
+	siteMetadata: require("./site-meta-data.json"),
+	plugins: [
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `markdown-pages`,
+				path: `${__dirname}/_data`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {
+				plugins: [
+					{
+						resolve: `gatsby-remark-prismjs`,
+						options: {
+							classPrefix: "language-",
+							inlineCodeMarker: null,
+							aliases: {},
+							showLineNumbers: false,
+							noInlineHighlight: false,
+						},
+					},
+					{
+						resolve: "gatsby-remark-emojis",
+					},
+				],
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// The property ID; the tracking code won't be generated without it. replace with yours
+				trackingId: "UA-164289266-2",
+				head: true,
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: `mashtech`,
+				short_name: `mashtech`,
+				start_url: `/`,
+				background_color: `#fff`,
+				theme_color: `#AEA851`,
+				display: `standalone`,
+				icon: "src/images/icon.png",
+			},
+		},
+		`gatsby-plugin-sass`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-netlify-cms`,
+		"gatsby-plugin-dark-mode",
+		// siteURL is a must for sitemap generation
+		`gatsby-plugin-sitemap`,
+		`gatsby-plugin-offline`,
+	],
 };
