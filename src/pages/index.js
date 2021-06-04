4dc627f692b28b162a96e894c1ad986e69e43eb3
marusho <mrsho34@gmail.com>
2021-06-04 19:02:36 +0900

    feat: ogp in idex

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { graphql } from "gatsby";
 import Layout from "../components/layout";
 import PostLink from "../components/post-link";
 import HeroHeader from "../components/heroHeader";
+import ogp_image from "../images/og.jpg";
 
 const IndexPage = ({
   data: {
@@ -16,40 +17,58 @@ const IndexPage = ({
     .map((edge) => <PostLink key={edge.node.id} post={edge.node} />);
 
   return (
-    <Layout>
-      <Helmet>
-        <title>{site.siteMetadata.title}</title>
-        <meta name="description" content={site.siteMetadata.description} />
-      </Helmet>
-      <HeroHeader />
-      <h2>Blog Posts &darr;</h2>
-      <div className="grids">{Posts}</div>
-    </Layout>
-  );
+		<Layout>
+			<Helmet>
+				<title>{site.siteMetadata.title}</title>
+				<meta name="description" content={site.siteMetadata.description} />
+				<meta property="og:site_name" content={site.siteMetadata.title} />
+				<meta
+					property="og:image"
+					content={`${site.siteMetadata.siteUrl}${ogp_image}`}
+				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:title" content={site.siteMetadata.title} />
+				<meta
+					name="twitter:description"
+					content={site.siteMetadata.description}
+				/>
+				<meta
+					name="twitter:image"
+					content={`${site.siteMetadata.siteUrl}${ogp_image}`}
+				/>
+			</Helmet>
+			<HeroHeader />
+			<h2>Blog Posts &darr;</h2>
+			<div className="grids">{Posts}</div>
+		</Layout>
+	);
 };
 
 export default IndexPage;
 export const pageQuery = graphql`
-  query indexPageQuery {
-    site {
-      siteMetadata {
-        title
-        description
-      }
-    }
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-            thumbnail
-          }
-        }
-      }
-    }
-  }
-`;
+					query indexPageQuery {
+						site {
+							siteMetadata {
+								title
+								description
+								siteUrl
+							}
+						}
+						allMarkdownRemark(
+							sort: { order: DESC, fields: [frontmatter___date] }
+						) {
+							edges {
+								node {
+									id
+									excerpt(pruneLength: 250)
+									frontmatter {
+										date(formatString: "MMMM DD, YYYY")
+										path
+										title
+										thumbnail
+									}
+								}
+							}
+						}
+					}
+				`;
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -21,7 +21,7 @@ export default function Template({
 				<meta property="og:site_name" content={siteMetadata.title} />
 				<meta
 					property="og:image"
-					content={`${data.site.siteMetadata.siteUrl}${ogp_image}`}
+					content={`${siteMetadata.siteUrl}${ogp_image}`}
 				/>
 				<meta name="twitter:card" content="summary_large_image" />
 				<meta name="twitter:title" content={frontmatter.title} />
@@ -31,7 +31,7 @@ export default function Template({
 				/>
 				<meta
 					name="twitter:image"
-					content={`${data.site.siteMetadata.siteUrl}${ogp_image}`}
+					content={`${siteMetadata.siteUrl}${ogp_image}`}
 				/>
 			</Helmet>
 			<div className="blog-post-container">
