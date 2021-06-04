3bcbc9bed5df62bc219fad4afd0932712edb04d7
marusho <mrsho34@gmail.com>
2021-06-04 17:32:47 +0900

    feat: add SNS share

---
+++ src/components/sns-section.js
@@ -0,0 +1,27 @@
+import {
+	FacebookShareButton,
+	FacebookIcon,
+	LineShareButton,
+	LineIcon,
+	TwitterShareButton,
+	TwitterIcon,
+} from "react-share";
+
+const SNSSection = ({ title, articleUrl }) => {
+	return (
+		<div>
+			<FacebookShareButton url={articleUrl}>
+				<FacebookIcon size={30} round />
+			</FacebookShareButton>
+
+			<LineShareButton url={articleUrl}>
+				<LineIcon size={30} round />
+			</LineShareButton>
+
+			<TwitterShareButton title={title} via="@marusho_summers" url={articleUrl}>
+				<TwitterIcon size={30} round />
+			</TwitterShareButton>
+		</div>
+	);
+};
+export default SNSSection;
--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -2,6 +2,7 @@ import React from "react"
 import Helmet from 'react-helmet';
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SNSSection from "../components/sns-section";
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -10,34 +11,39 @@ export default function Template({
   const { siteMetadata } = site
   const { frontmatter, html } = markdownRemark
   return (
-    <Layout>
-      <Helmet>
-        <title>{frontmatter.title} | {siteMetadata.title}</title>
-        <meta name="description" content={frontmatter.metaDescription} />
-      </Helmet>
-      <div className="blog-post-container">
-        <article className="post">
-          
-          {!frontmatter.thumbnail && (
-            <div className="post-thumbnail">
-              <h1 className="post-title">{frontmatter.title}</h1>
-              <div className="post-meta">{frontmatter.date}</div>
-            </div>
-          )}
-          {!!frontmatter.thumbnail && (
-            <div className="post-thumbnail" style={{backgroundImage: `url(${frontmatter.thumbnail})`}}>
-              <h1 className="post-title">{frontmatter.title}</h1>
-              <div className="post-meta">{frontmatter.date}</div>
-            </div>
-          )}
-          <div
-            className="blog-post-content"
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </article>
-      </div>
-    </Layout>
-  )
+		<Layout>
+			<Helmet>
+				<title>
+					{frontmatter.title} | {siteMetadata.title}
+				</title>
+				<meta name="description" content={frontmatter.metaDescription} />
+			</Helmet>
+			<div className="blog-post-container">
+				<article className="post">
+					{!frontmatter.thumbnail && (
+						<div className="post-thumbnail">
+							<h1 className="post-title">{frontmatter.title}</h1>
+							<div className="post-meta">{frontmatter.date}</div>
+						</div>
+					)}
+					{!!frontmatter.thumbnail && (
+						<div
+							className="post-thumbnail"
+							style={{ backgroundImage: `url(${frontmatter.thumbnail})` }}
+						>
+							<h1 className="post-title">{frontmatter.title}</h1>
+							<div className="post-meta">{frontmatter.date}</div>
+						</div>
+					)}
+					<div
+						className="blog-post-content"
+						dangerouslySetInnerHTML={{ __html: html }}
+					/>
+				</article>
+			</div>
+			<SNSSection title={frontmatter.title} url={frontmatter.path} />
+		</Layout>
+	);
 }
 
 export const pageQuery = graphql`
