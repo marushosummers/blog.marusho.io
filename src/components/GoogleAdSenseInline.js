f888486063d27af1dcd149d2a476c9cc7179fbc8
marusho <mrsho34@gmail.com>
2021-12-15 15:22:28 +0900

    adsense

--- src/components/GoogleAdSenseInline.js
+++ src/components/GoogleAdSenseInline.js
@@ -16,6 +16,7 @@ export default function AdsInlineCard({ path }, slot) {
 				style={{ display: "inline-block", width: "320px", height: "100px" }}
 				data-ad-client="ca-pub-7961076646821939"
 				data-ad-slot={slot}
+				data-full-width-responsive="false"
 			></ins>
 		</>
 	);
